599b3968f7db796d426cd07e8dae67894c62de78
Nataliia Lyfar <lifarnatala@gmail.com>
2022-05-03 13:20:51 +0300

    fix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,28 +2,33 @@ import throttle from "lodash.throttle";
 
 const form = document.querySelector('.feedback-form');
 const LOCALSTORAGE_KEY = "feedback-form-state";
-const formData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) ?? {};
-
-form.elements.email.value = formData.email ?? "";
-form.elements.message.value = formData.message ?? "";
+let data = {};
+console.log(form);
 
 const onInputDataSave = (e) => {
-    formData[e.target.name] = e.target.value;
-    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+    data[e.target.name] = e.target.value;
+    const storageDate = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify({ ...storageDate, ...data }));
 };
 
 const onFormSubmit = (e) => {
     e.preventDefault(); 
-    const formElements = e.currentTarget.elements;
-    const email = formElements.email.value;
-    const message = formElements.message.value;
-
-    const  formData = { email, message };
-    console.log(formData);
-    
-    localStorage.removeItem(LOCALSTORAGE_KEY);
-    e.currentTarget.reset();
+    if (form.elements.email.value == '' || form.elements.message.value == '') {
+        alert('Error, all fields must be filled!!');
+        return;
+    } 
+        e.currentTarget.reset();
+        console.log(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)));
+        localStorage.removeItem(LOCALSTORAGE_KEY);
+        data = {};
 };
 
-form.addEventListener('input', onInputDataSave);
-form.addEventListener('submit', throttle(onFormSubmit, 500));
+function savedDataOutput () {
+    const savedDataForm = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+    Object.keys(savedDataForm || {}).forEach((item) => form[item].value = savedDataForm[item]);
+};
+
+form.addEventListener('input', throttle(onInputDataSave,500));
+form.addEventListener('submit', onFormSubmit);
+savedDataOutput();
+
